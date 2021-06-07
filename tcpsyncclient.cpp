f60202fa4f4710464681f39f5e455b46279f0a67
acetone <acetone@mail.i2p>
2021-06-07 18:34:33 +0300

    +B flag (for Bot ident

--- tcpsyncclient.cpp
+++ tcpsyncclient.cpp
@@ -127,6 +127,7 @@ void TcpSyncClient::connect_to_server()
         write("PRIVMSG NICKSERV IDENTIFY " + params["password"]);
     }
     write("JOIN " + params["channel"]);
+    write("MODE " + m_user + " +B"); // флаг бота
 }
 
 void TcpSyncClient::start()
